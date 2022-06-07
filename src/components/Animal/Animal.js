cb220376d6cb356a98d3043dd5449d0f344aa17f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-07 14:54:08 -0700

    renders images to page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -5,7 +5,7 @@ import './Animal.css';
 export default function Animal({ name, type, says }) {
   return (
     <div className="animal-card">
-      <img></img>
+      <img src={`/images/${type}.png`}/>
       <p className="name">{name}</p>
       <p>{says}</p>
     </div>
