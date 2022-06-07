5716351449bb7eb8fb414e2398688b6eb18664f9
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-07 13:08:51 -0700

    adds Animals function and puts Animal info into card, renders to page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,6 +1,13 @@
-
 import React from 'react';
+import './Animal.css';
 
-export default function Animal() {
-  return <></>;
+
+export default function Animal({ name, type, says }) {
+  return (
+    <div className="animal-card">
+      <img></img>
+      <p className="name">{name}</p>
+      <p>{says}</p>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Main.css';
 import backgroundImg from '../../background.png';
 //import animal card here 
+import Animal from '../Animal/Animal.js';
 //import animal data here 
+import { animals } from '../../data.js';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      
+      {
+        animals.map((animals, i) => <Animal
+          {...animals}
+          key={animals.name + i}/>)
+      }
+
     </main>
   );
 }
